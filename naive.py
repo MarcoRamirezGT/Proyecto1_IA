dc70fbeab854e380d290aa836283fb0e925071c2
MarcoRamirezGT <maquito156@gmail.com>
2022-05-24 20:03:09 -0600

    Update naive.py

--- naive.py
+++ naive.py
@@ -10,6 +10,10 @@ import time
 start_time = time.time()
 
 df = pd.read_csv('Fraud.csv')
+df_2 = df[df['isFraud'] == 0]
+df_3 = df_2.head(8213)
+df_1 = df[df['isFraud'] == 1]
+df = pd.concat([df_1, df_3])
 
 X = df[['amount', 'oldbalanceOrg', 'newbalanceOrig',
         'oldbalanceDest', 'newbalanceDest']]
